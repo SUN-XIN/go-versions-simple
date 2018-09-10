3ebf234c49bbb5bcbc0becb98ec066457840957b
Administrator <xin.sun36@gmail.com>
2018-09-10 16:31:40 +0200

    feat(simple-1.9/type-alias):switch+method

--- simple-1.9/type-alias/main.go
+++ simple-1.9/type-alias/main.go
@@ -10,6 +10,10 @@ type Person struct {
 	Name string
 }
 
+func (p Person) Explain() string {
+	return fmt.Sprintf("%s is %d years old", p.Name, p.Age)
+}
+
 func main() {
 	type Employer = Person
 
@@ -25,6 +29,36 @@ func main() {
 
 	fmt.Println("Type Person=Employer ? ", emp == pers)
 
+	//////////////////////////////////////////////////////////////////////////////
+	////////////////////////////   TypeOf    /////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////
 	fmt.Println("TypeOf Person", reflect.TypeOf(pers))
 	fmt.Println("TypeOf Employer", reflect.TypeOf(emp))
+
+	//////////////////////////////////////////////////////////////////////////////
+	////////////////////////////   Switch    /////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////
+	var interfEmployer interface{}
+	interfEmployer = emp
+	switch t := interfEmployer.(type) {
+	// case Person, Employer: -> ERROR! duplicate case Person in type switch
+	case Person:
+		fmt.Println("switch Employer.(type) -> Person")
+	default:
+		fmt.Println("switch Employer.(type) -> %v", t)
+	}
+
+	var interfPerson interface{}
+	interfPerson = pers
+	switch t := interfPerson.(type) {
+	case Employer:
+		fmt.Println("switch Person.(type) -> Employer")
+	default:
+		fmt.Println("switch Person.(type) -> %v", t)
+	}
+
+	//////////////////////////////////////////////////////////////////////////////
+	////////////////////////////   Method    /////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////
+	fmt.Println("Employer uses Person's method:", emp.Explain())
 }
